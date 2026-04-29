2e8df5e925e1203ebee408f5f53ecc7f0d9891f5
adiraje7 <adiraje877@gmail.com>
2026-04-29 19:22:35 +0530

    Add SimpleModel class with a convolutional layer

--- src/models/model.py
+++ src/models/model.py
@@ -1,1 +1,9 @@
+import torch.nn as nn
 
+class SimpleModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.net = nn.Conv2d(3, 4, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        return self.net(x)

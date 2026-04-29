974dc05b3073849cbc7a8e64a18c7b52896282fd
adiraje7 <adiraje877@gmail.com>
2026-04-29 19:22:13 +0530

    Add DummyDataset class for data loading

--- src/data/loader.py
+++ src/data/loader.py
@@ -1,1 +1,11 @@
+import torch
+from torch.utils.data import Dataset
 
+class DummyDataset(Dataset):
+    def __len__(self):
+        return 100
+
+    def __getitem__(self, idx):
+        x = torch.randn(3, 224, 224)
+        y = torch.randint(0, 4, (224, 224))
+        return x, y
